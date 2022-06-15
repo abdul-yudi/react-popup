1919f6181b2b02e1a576d2ca79b0e6aef4f8dc06
Yudi <abd.rahmat.wahyudi@gmail.com>
2022-06-15 15:49:21 +0700

    add modal header and footer props

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import ReactDom from "react-dom";
 
-export const Modal = ({ setShowModal, contentModal }) => {
+export const Modal = ({ setShowModal, modalHeader, contentModal, modalFooter }) => {
   const modalRef = useRef();
 
   const closeModal = (e) => {
@@ -15,13 +15,14 @@ export const Modal = ({ setShowModal, contentModal }) => {
     <div className="container" ref={modalRef} onClick={closeModal}>
       <div className="modal">
         <div className="modal-header">
-          Modal Header
+          {modalHeader}
           <button className="button close" onClick={() => setShowModal(false)}>X</button>
         </div>
         <div className="modal-body">
           {contentModal}
         </div>
         <div className="modal-footer">
+          {modalFooter}
           <button className="button" onClick={() => setShowModal(false)}>Close</button>
         </div>
       </div>
